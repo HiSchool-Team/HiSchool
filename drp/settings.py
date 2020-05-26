16caea38d8dd7951c0eceb12add080be32052ddb
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-05-26 18:36:25 +0100

    Remove plaintext secret key

--- drp/settings.py
+++ drp/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')0+$fi5gzktob#$p^ztu40if3sw8^etrof^=0x&x(qh%0v+l9^'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
