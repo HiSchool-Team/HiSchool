561e45cc702be2669b30e05d4812b61845e9f402
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-05-28 23:50:20 +0100

    Fix routing to react

--- backend/drp/urls.py
+++ backend/drp/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('app.urls')),
     path('admin/', admin.site.urls),
-    path('react/', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    url(r'^.*', TemplateView.as_view(template_name='index.html'), name='unmatched')
 ]
