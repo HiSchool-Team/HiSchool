6c1d41a12b944610bdf370ef71f3cad78de7ccbe
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-06-02 16:10:08 +0100

    [Q&A] Further implement layout for Q&A cards Co-Authored-By: Andreas Casapu <ac9018@ic.ac.uk>

--- frontend/src/components/QA.tsx
+++ frontend/src/components/QA.tsx
@@ -1,30 +1,41 @@
 import React from 'react'
 import { Answer, Question } from '../types'
 import { StarRating } from './Rating'
+import { Col, Row } from 'antd'
+import './QA.css'
 
 type AnswerProps = {
-    answer: Answer,
+  answer: Answer,
 }
 
 const AnswerComponent = ({ answer }: AnswerProps) => {
   return (
-    <div>
-      <h3>Answer</h3>
-      <p>{answer.body}</p>
-      <StarRating rating={answer.rating} />
-      {answer.teacher_name && <p>{answer.teacher_name}</p>}
+
+    <div className={'answer'}>
+      <Row>
+        <Col span={18}>
+          <h2>Answer</h2>
+          <p>{answer.body}</p>
+          {answer.teacher_name && <p>{answer.teacher_name}</p>}
+        </Col>
+        <Col span={6}>
+          <div className={'answer-rating'}>
+            <StarRating rating={answer.rating}/>
+          </div>
+        </Col>
+      </Row>
     </div>
   )
 }
 
 type Props = {
-    question: Question,
+  question: Question,
 }
 
 export const QA = ({ question }: Props) => {
   return (
     <div className={'qa'}>
-      <h3>{question.title}</h3>
+      <h2>{question.title}</h2>
       <p>{question.body}</p>
       {question.answer && <AnswerComponent answer={question.answer}/>}
     </div>
--- frontend/src/components/QAList.tsx
+++ frontend/src/components/QAList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { QA } from './QA'
-import { List } from 'antd'
+import { Card, List } from 'antd'
 import { Question } from '../types'
 
 export const QAList = (props: {data: Question[], }) => {
@@ -9,7 +9,7 @@ export const QAList = (props: {data: Question[], }) => {
       itemLayout="vertical"
       size="large"
       dataSource={props.data}
-      renderItem={(item: Question) => <QA question={item}/>}
+      renderItem={(item: Question) => <Card><QA question={item}/></Card>}
     />
   )
 }
