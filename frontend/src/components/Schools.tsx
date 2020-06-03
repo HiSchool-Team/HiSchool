4d6582c6c5995d73b7ea8ff504c4680e35737bd7
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-06-03 19:22:54 +0100

    [General] Fix stylesheet import in school list view

--- frontend/src/components/Schools.tsx
+++ frontend/src/components/Schools.tsx
@@ -1,60 +1,60 @@
 import React from 'react';
-import {Card, List, Space} from 'antd';
-import {StarOutlined} from '@ant-design/icons';
-import styles from './Schools.module.css';
-import {School} from '../types';
-import {v1 as uuidv4} from 'uuid';
+import { Card, List, Space } from 'antd';
+import { StarOutlined } from '@ant-design/icons';
+import './Schools.css';
+import { School } from '../types';
+import { v1 as uuidv4 } from 'uuid';
 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tellus justo. Vestibulum vestibulum turpis vel elit lacinia accumsan. Proin accumsan, purus nec congue lacinia, nisl nibh porttitor nunc, eget sagittis tellus metus vitae quam. Donec in porta dui. In hac habitasse platea dictumst. Curabitur id maximus purus, in commodo tellus. Ut orci purus, sagittis quis vulputate sit amet, pulvinar sit amet enim. Curabitur sit amet malesuada sem. Donec cursus ex eget turpis egestas, at tincidunt neque convallis. Maecenas dictum erat dui, et consectetur lorem convallis et.';
 
 // @ts-ignore
-const RepeatIcon = ({label, icon, times}) => {
-    const icons = [];
-    for (let i = 0; i < times; i++) {
-        icons.push(<div>{React.createElement(icon)}</div>);
-    }
+const RepeatIcon = ({ label, icon, times }) => {
+  const icons = [];
+  for (let i = 0; i < times; i++) {
+    icons.push(<div>{React.createElement(icon)}</div>);
+  }
 
-    return (
-        <div style={{
-            display: 'block',
-            textAlign: 'right',
-            transform: 'scale(1.2)',
-            marginLeft: '200px'
-        }}>
-            <Space><b>{label}</b>{icons}</Space>
-        </div>
-    );
+  return (
+    <div style={{
+      display: 'block',
+      textAlign: 'right',
+      transform: 'scale(1.2)',
+      marginLeft: '200px'
+    }}>
+      <Space><b>{label}</b>{icons}</Space>
+    </div>
+  );
 };
 
 type Props = {
     data: School[],
 };
 
-const Schools = ({data}: Props) => {
-    return (
-        <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={data}
-            renderItem={(item: School) => (
-                <Card>
-                    <List.Item key={uuidv4()}>
-                        <List.Item.Meta title={<a href={`/${item.id}`}><b>{item.name}</b></a>}/>
+const Schools = ({ data }: Props) => {
+  return (
+    <List
+      itemLayout="vertical"
+      size="large"
+      dataSource={data}
+      renderItem={(item: School) => (
+        <Card>
+          <List.Item key={uuidv4()}>
+            <List.Item.Meta title={<a href={`/${item.id}`}><b>{item.name}</b></a>}/>
 
-                        <div>
-                            <div className={styles.schoolPicture}><img alt="logo" src={item.img_src}/></div>
-                            <div className={styles.schoolDescription}><p>{loremIpsum}</p></div>
-                            <div className={styles.satisfactionLevels}>
-                                <RepeatIcon label={'Student satisfaction'} icon={StarOutlined} times={5}/>
-                                <RepeatIcon label={'Parent satisfaction'} icon={StarOutlined} times={5}/>
-                                <RepeatIcon label={'Extracurriculars'} icon={StarOutlined} times={5}/>
-                            </div>
-                        </div>
-                    </List.Item>
-                </Card>
-            )}
-        />
-    );
+            <div>
+              <div className={'school-picture'}><img alt="logo" src={item.img_src}/></div>
+              <div className={'school-description'}><p>{loremIpsum}</p></div>
+              <div className={'satisfaction-levels'}>
+                <RepeatIcon label={'Student satisfaction'} icon={StarOutlined} times={5}/>
+                <RepeatIcon label={'Parent satisfaction'} icon={StarOutlined} times={5}/>
+                <RepeatIcon label={'Extracurriculars'} icon={StarOutlined} times={5}/>
+              </div>
+            </div>
+          </List.Item>
+        </Card>
+      )}
+    />
+  );
 };
 
 export default Schools;
