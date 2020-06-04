37411dce931d98cf48c0916bf5f9a355b09ed36d
rp218 <rp218@imperial.ac.uk>
2020-06-04 19:46:10 +0200

    [Search] Inserts school description on main search page
    Co-authored-by: Lazar <ll8618@ic.ac.uk>

--- frontend/src/components/Schools.tsx
+++ frontend/src/components/Schools.tsx
@@ -5,8 +5,6 @@ import styles from './Schools.module.css';
 import {School} from '../types';
 import {v1 as uuidv4} from 'uuid';
 
-const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tellus justo. Vestibulum vestibulum turpis vel elit lacinia accumsan. Proin accumsan, purus nec congue lacinia, nisl nibh porttitor nunc, eget sagittis tellus metus vitae quam. Donec in porta dui. In hac habitasse platea dictumst. Curabitur id maximus purus, in commodo tellus. Ut orci purus, sagittis quis vulputate sit amet, pulvinar sit amet enim. Curabitur sit amet malesuada sem. Donec cursus ex eget turpis egestas, at tincidunt neque convallis. Maecenas dictum erat dui, et consectetur lorem convallis et.';
-
 // @ts-ignore
 const RepeatIcon = ({label, icon, times}) => {
     const icons = [];
@@ -43,7 +41,7 @@ const Schools = ({data}: Props) => {
 
                         <div>
                             <div className={styles.schoolPicture}><img alt="logo" src={item.img_src}/></div>
-                            <div className={styles.schoolDescription}><p>{loremIpsum}</p></div>
+                            <div className={styles.schoolDescription}><p>{item.description}</p></div>
                             <div className={styles.satisfactionLevels}>
                                 <RepeatIcon label={'Student satisfaction'} icon={StarOutlined} times={5}/>
                                 <RepeatIcon label={'Parent satisfaction'} icon={StarOutlined} times={5}/>
