98682e1030d76546c0d47b59e1c17dfd3caebb24
rp218 <rp218@imperial.ac.uk>
2020-06-09 13:30:11 +0200

    [General] Adds utils file with funtions for redirection and parsing search result
    Co-authored-by: Lazar <ll8618@ic.ac.uk>

---
+++ frontend/src/utils/utils.tsx
@@ -0,0 +1,17 @@
+import {Location} from 'history';
+import history from "./history";
+
+export const getSearchResult = (location: Location): string => {
+  const qs = require('qs');
+  const queryParams = qs.parse(location.search, {ignoreQueryPrefix: true});
+  return queryParams.search;
+};
+
+export const goToNewUrl = (basePath: string,
+                           searchParams: string[][] | Record<string, string> | string | URLSearchParams): void => {
+  // https://stackoverflow.com/questions/42701129/how-to-push-to-history-in-react-router-v4
+  history.push({
+    pathname: basePath,
+    search: `?${new URLSearchParams(searchParams)}`
+  });
+}
