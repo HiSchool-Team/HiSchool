6084fa62d60cbbc69a92d6f73dd555ffd3f9b8e8
rp218 <rp218@imperial.ac.uk>
2020-06-11 16:28:39 +0200

    [Search] Tags can be selected
    Co-authored-by: Lazar <ll8618@ic.ac.uk>

--- frontend/src/containers/views/PreferenceChoices.tsx
+++ frontend/src/containers/views/PreferenceChoices.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 
 import './PreferenceChoices.css';
 
@@ -8,73 +8,92 @@ import history from '../../utils/history';
 import ActionButton from "antd/es/modal/ActionButton";
 import NewLayout from "../NewLayout";
 import Search from "antd/es/input/Search";
+import {Tag} from '../../components/Tag';
 
 // TODO check if this import is needed
 
-const { Header, Content, Sider} = Layout;
-const { TabPane } = Tabs;
+const {Header, Content, Sider} = Layout;
+const {TabPane} = Tabs;
 
-const PreferenceChoices = (props: {children: React.ReactNode,
-    handleSearch(): any;
+const PreferenceChoices = (props: {
+  children: React.ReactNode,
+  handleSearch(): any;
 }) => {
 
-    let options = [
-                  {
-                    value: 'Football Club',
-                  },
-                  {
-                    value: 'Drama Club',
-                  },
-                  {
-                    value: 'Breakfast Club',
-                  },
-                ];
+  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set<string>());
 
-    const onSelect = (value:string) => {
+  const toggleTagState = (selectedTag: string) => {
+    const newSelected = new Set<string>(selectedTags);
+    if (!newSelected.delete(selectedTag)) {
+      newSelected.add(selectedTag);
+    }
+    setSelectedTags(newSelected);
+  }
 
-    };
+  let options = [
+    {
+      value: 'Football Club',
+    },
+    {
+      value: 'Drama Club',
+    },
+    {
+      value: 'Breakfast Club',
+    },
+  ];
 
-    return(
-        <NewLayout>
-            <div className="grid-container">
-                <div className={"head-search"}>
-                    Search here for extracurricular/pastoral you would prefer the School contains<br/>
-                    <AutoComplete
-                        options = {options}
-                        style={{
-                            width: 200,
-                        }}
-                        onSelect={onSelect}
-                        placeholder="input here"
-                    />
-                </div>
+  const onSelect = (value: string) => {
 
-                <div>
-                    <Tabs type="card">
-                        <TabPane tab="Sports" key="1">
-                            <button className="button">Football Club</button>
-                            <button className="button">Cricket Club</button>
-                            <button className="button">Chess Club</button>
-                        </TabPane>
-                      <TabPane tab="Science" key="2">
-                          <button className="button">Astronomy Club</button>
-                          <button className="button">Arduino Society</button>
-                          <button className="button">SAT Preparation</button>
-                      </TabPane>
-                      <TabPane tab="Art" key="3">
-                          <button className="button">Sculpt</button>
-                      </TabPane>
-                    </Tabs>
-                </div>
+  };
 
-                <div>
-                    <div className={"preference-search"}>
-                      <a href={`${window.parent.location}list`} className="btn btn-primary">Find me a School</a>
-                    </div>
-                </div>
-            </div>
-        </NewLayout>
-    );
+  return (
+    <NewLayout>
+      <div className="grid-container">
+        <div className={"head-search"}>
+          Search here for extracurricular/pastoral you would prefer the School contains<br/>
+          <AutoComplete
+            options={options}
+            style={{
+              width: 200,
+            }}
+            onSelect={onSelect}
+            placeholder="input here"
+          />
+        </div>
+
+        <div>
+          <Tabs type="card">
+            <TabPane tab="Sports" key="1">
+              <Tag name={"Football club"} onClick={() => toggleTagState("Football club")}
+                   selected={selectedTags.has("Football club")}/>
+              <Tag name={"Chess club"} onClick={() => toggleTagState("Chess club")}
+                   selected={selectedTags.has("Chess club")}/>
+              <Tag name={"Cricket club"} onClick={() => toggleTagState("Cricket club")}
+                   selected={selectedTags.has("Cricket club")}/>
+            </TabPane>
+            <TabPane tab="Science" key="2">
+              <Tag name={"Astronomy Club"} onClick={() => toggleTagState("Astronomy Club")}
+                   selected={selectedTags.has("Astronomy Club")}/>
+              <Tag name={"Arduino Society"} onClick={() => toggleTagState("Arduino Society")}
+                   selected={selectedTags.has("Arduino Society")}/>
+              <Tag name={"SAT Preparation"} onClick={() => toggleTagState("SAT Preparation")}
+                   selected={selectedTags.has("SAT Preparation")}/>
+            </TabPane>
+            <TabPane tab="Art" key="3">
+              <Tag name={"Sculpt"} onClick={() => toggleTagState("Sculpt")}
+                   selected={selectedTags.has("Sculpt")}/>
+            </TabPane>
+          </Tabs>
+        </div>
+
+        <div>
+          <div className={"preference-search"}>
+            <a href={`${window.parent.location}list`} className="btn btn-primary">Find me a School</a>
+          </div>
+        </div>
+      </div>
+    </NewLayout>
+  );
 }
 
 export default PreferenceChoices;
