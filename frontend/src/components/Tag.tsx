a283a0002dfa80bf35b68942f1ff91dedbdb6004
rp218 <rp218@imperial.ac.uk>
2020-06-11 14:51:36 +0200

    [General] Tags have ability to be selected
    Co-authored-by: Lazar <ll8618@ic.ac.uk>

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,14 +1,14 @@
 import React, {CSSProperties, MouseEvent, FocusEvent} from "react";
 
-export const Tag = (props: {style?: CSSProperties,
-                            name: string, selected?: boolean,
-                            onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void,
-                            onMouseLeave?: (e: MouseEvent<HTMLDivElement>) => void,
-                            onClick?: (e: MouseEvent<HTMLDivElement>) => void,
-                            onFocus?: (e: FocusEvent<HTMLDivElement>) => void,
-                           }) => {
+export const Tag = (props: {
+  style?: CSSProperties,
+  name: string, selected?: boolean,
+  onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void,
+  onMouseLeave?: (e: MouseEvent<HTMLDivElement>) => void,
+  onClick?: (e: MouseEvent<HTMLDivElement>) => void,
+  onFocus?: (e: FocusEvent<HTMLDivElement>) => void,
+}) => {
   const pillStyle: CSSProperties = {
-    backgroundColor: "#ddd",
     border: "none",
     padding: "10px 20px",
     textAlign: "center",
@@ -19,12 +19,18 @@ export const Tag = (props: {style?: CSSProperties,
     borderRadius: "16px",
   }
 
-  return <div style={{...pillStyle, ...props.style}}
+  const selected: CSSProperties = {
+    backgroundColor: props.selected ? "#999898" : "#c7c6c6",
+    color: props.selected ? "#ece9e9" : "#242424",
+  }
+
+
+  return <div style={{...pillStyle, ...props.style, ...selected}}
               onMouseEnter={props.onMouseEnter}
               onMouseLeave={props.onMouseLeave}
               onClick={props.onClick}
               onFocus={props.onFocus}
-              >
+  >
     {props.name}
   </div>
 }
