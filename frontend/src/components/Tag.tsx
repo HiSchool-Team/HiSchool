ed950d6b26d09b8f32546ecdf8f68835f9ad676f
rp218 <rp218@imperial.ac.uk>
2020-06-15 21:33:04 +0200

    [Preferences] Implements adding tags to drag and drop zone
    Co-authored-by: Lazar <ll8618@ic.ac.uk>

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,12 +1,20 @@
-import React, { CSSProperties, MouseEvent, FocusEvent } from 'react';
+import React, {CSSProperties, MouseEvent, FocusEvent} from 'react';
+import {DragSourceMonitor, useDrag} from "react-dnd";
+import {Tag as TagType} from "../types";
+
+export const ItemTypes = {
+  TAG: 'tag',
+}
 
 export const Tag = (props: {
+  id: number,
   style?: CSSProperties,
   name: string, selected?: boolean,
   onMouseEnter?: (e: MouseEvent<HTMLDivElement>) => void,
   onMouseLeave?: (e: MouseEvent<HTMLDivElement>) => void,
   onClick?: (e: MouseEvent<HTMLDivElement>) => void,
   onFocus?: (e: FocusEvent<HTMLDivElement>) => void,
+  onDrop?: (id: number) => void,
 }) => {
   const pillStyle: CSSProperties = {
     border: 'none',
@@ -16,20 +24,35 @@ export const Tag = (props: {
     display: 'inline-block',
     margin: '4px 2px',
     cursor: 'pointer',
-    borderRadius: '16px'
+    borderRadius: '16px',
   };
 
   const selected: CSSProperties = {
     backgroundColor: props.selected ? '#001529' : '#c7c6c6',
     color: props.selected ? '#ece9e9' : '#242424'
   };
+  const [{ isDragging }, drag] = useDrag({
+    item: { name: props.name, type: ItemTypes.TAG},
+    end: (item: { name: string } | undefined, monitor: DragSourceMonitor) => {
+      const dropResult = monitor.getDropResult()
+      if (item && dropResult) {
+        props.onDrop?.(props.id);
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0.4 : 1
 
-  return <div style={{ ...pillStyle, ...props.style, ...selected }}
-    onMouseEnter={props.onMouseEnter}
-    onMouseLeave={props.onMouseLeave}
-    onClick={props.onClick}
-    onFocus={props.onFocus}
-  >
-    {props.name}
-  </div>;
+  return (
+    <div ref={drag}
+         style={{...pillStyle, ...props.style, ...selected, opacity}}
+         onMouseEnter={props.onMouseEnter}
+         onMouseLeave={props.onMouseLeave}
+         onClick={props.onClick}
+         onFocus={props.onFocus}
+    >
+      {props.name}
+    </div>);
 };
--- frontend/src/containers/views/PreferenceChoices.tsx
+++ frontend/src/containers/views/PreferenceChoices.tsx
@@ -1,4 +1,4 @@
-import React, {FormEvent, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './PreferenceChoices.css';
 
@@ -8,8 +8,11 @@ import {School, Tag} from '../../types';
 import {Tag as TagComponent} from '../../components/Tag';
 import axios, {AxiosResponse} from 'axios';
 import {serverSearchEndpoint} from "./SchoolList";
-import { calculatePreferences } from '../../logic/Search';
-import Search from "antd/lib/transfer/search";
+import {calculatePreferences} from '../../logic/Search';
+import DragDropZone from "../../components/DragDropZone";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
+import Search from 'antd/lib/transfer/search';
 
 // TODO check if this import is needed
 
@@ -21,16 +24,42 @@ interface ServerResponse {
   tags: Tag[],
 }
 
-const PreferenceChoices = (props: {
-  children: React.ReactNode,
-  handleSearch(): any;
-}) => {
+type ReducerState = {
+  dropDepth: number,
+  inDropZone: boolean,
+  tagsIn: Tag[],
+}
+
+type ReducerAction =
+  | { type: 'SET_DROP_DEPTH', dropDepth: number }
+  | { type: 'SET_IN_DROP_ZONE', inDropZone: boolean }
+  | { type: 'ADD_TAG_TO_LIST', tagToAdd: Tag };
+
+const PreferenceChoices = () => {
 
   const [availableTags, setAvailableTags] = useState<Tag[]>([]);
   const [availableCategories, setAvailableCategories] = useState<string[]>([]);
-  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set<string>());
-  const [selectedTypeTags, setSelectedTypeTags] = useState<string[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
+  const [insertedTags, setInsertedTags] = useState<number[]>([]);
+  const [selectedTypeTags, setSelectedTypeTags] = useState<string[]>([]);
+
+  // TODO types
+  const reducer = (state: ReducerState, action: ReducerAction) => {
+    switch (action.type) {
+      case 'SET_DROP_DEPTH':
+        return {...state, dropDepth: action.dropDepth}
+      case 'SET_IN_DROP_ZONE':
+        return {...state, inDropZone: action.inDropZone}
+      case 'ADD_TAG_TO_LIST':
+        return {...state, tagsIn: state.tagsIn.concat(action.tagToAdd)}
+      default:
+        return state;
+    }
+  }
+
+  const [data, dispatch] = React.useReducer(reducer, {
+    dropDepth: 0, inDropZone: false, tagsIn: []
+  })
 
   useEffect(() => {
     axios.get<ServerResponse>(serverSearchEndpoint, {
@@ -39,18 +68,23 @@ const PreferenceChoices = (props: {
       setAvailableTags(resp.data.tags);
       setAvailableCategories(
         resp.data.tags.filter(tag => tag.sub_type !== 'General')
-                      .map(tag => tag.sub_type)
-                      .filter((tag, i, self) => self.indexOf(tag) === i)
+          .map(tag => tag.sub_type)
+          .filter((tag, i, self) => self.indexOf(tag) === i)
       );
     })
   }, [])
 
-  const toggleTagState = (selectedTag: string) => {
-    const newSelected = new Set<string>(selectedTags);
-    if (!newSelected.delete(selectedTag)) {
-      newSelected.add(selectedTag);
-    }
-    setSelectedTags(newSelected);
+  const getComponentsByRelevance = (category: string, givenString: string) => {
+    const categoryTags = nonDroppedTags.filter(tag => tag.sub_type === category);
+    const tagsPreferences = calculatePreferences(givenString, categoryTags);
+
+    return <>
+      {tagsPreferences
+        .map(([tag, value]) => {
+          return <TagComponent id={tag.id} name={tag.name + "(" + value + ")"}
+                               onDrop={(id: number) => setInsertedTags(prevState => [...prevState, id])} />
+        })}
+    </>;
   }
 
   const addSelectedTypeTags = (selectedTags: number[]) => {
@@ -64,69 +98,51 @@ const PreferenceChoices = (props: {
     setSelectedTypeTags(newTypeTags);
   }
 
-  let options = [
-    {
-      value: 'Football Club',
-    },
-    {
-      value: 'Drama Club',
-    },
-    {
-      value: 'Breakfast Club',
-    },
-  ];
-
-
-  const getComponentsByRelevance = (category: string, givenString: string) => {
-    const categoryTags = availableTags.filter(tag => tag.sub_type === category);
-    const tagsPreferences = calculatePreferences(givenString, categoryTags);
-
-    return <>
-      {tagsPreferences
-        .map(([tag, value]) => {
-          return <TagComponent name={tag.name + "(" + value + ")"} onClick={() => toggleTagState(tag.name)}
-                               selected={selectedTags.has(tag.name)}/>
-        })}
-    </>;
-  }
-
   const onSelect = () => {
 
   }
 
-  return (
-    <NewLayout tags={availableTags.filter(tag => tag.sub_type === 'General')}
-               updateDisplayedSchool={addSelectedTypeTags}>
-      <div className="grid-container">
-          <div className={"tab-display"}>
-          <Tabs type="card" className={"tabs"}>
-            {availableCategories.map(category => {
-              return <TabPane tab={category} key={category}>
-                {getComponentsByRelevance(category, searchValue)}
-              </TabPane>
-            })}
-          </Tabs>
-        </div>
+  const droppedTags = availableTags.filter(tag => insertedTags.includes(tag.id));
+  const nonDroppedTags = availableTags.filter(tag => !insertedTags.includes(tag.id));
 
-        <div className={"head-search"}>
-          Search here for your School extracurricular preference<br/>
-          <Search placeholder={'search tags'}
-                  onChange={(e: FormEvent<HTMLElement>) => {
-                    let elem: HTMLInputElement = e.currentTarget as HTMLInputElement;
-                    setSearchValue(elem.value);
-                  }}/>
-          <br/><br/>
-          Click on the tags which you wish to select on the left
-          <br/>
-          <img src="/static/help.png" alt={"help"}/>
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          After selecting the tags you prefer search for a suitable school by clicking on the button bellow
-          <div className={"preference-search"}>
-            <a href={`/1`} className="button">Find me a School</a>
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <NewLayout tags={availableTags.filter(tag => tag.sub_type === 'General')}
+                 updateDisplayedSchool={addSelectedTypeTags}>
+        <div className="grid-container">
+          <div className={"tab-display"}>
+            <TagComponent id={0} name={"test"}
+                               onDrop={(id: number) => {console.log(insertedTags); setInsertedTags(prevState => [...prevState, id]); console.log(id)}} />
+            <Tabs type="card" className={"tabs"}>
+              {availableCategories.map(category => {
+                return <TabPane tab={category} key={category}>
+                  {getComponentsByRelevance(category, searchValue)}
+                </TabPane>
+              })}
+            </Tabs>
+          </div>
+
+          <div className={"head-search"}>
+            Search here for your School extracurricular preference<br/>
+            <DragDropZone tags={droppedTags}/>
+            <Search placeholder={'search tags'}
+                    onChange={(e) => {
+                      let elem: HTMLInputElement = e.currentTarget as HTMLInputElement;
+                      setSearchValue(elem.value);
+                    }}/>
+            <br/><br/>
+            Click on the tags which you wish to select on the left
+            <br/>
+            <img src="/static/help.png" alt={"help"}/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            After selecting the tags you prefer search for a suitable school by clicking on the button bellow
+            <div className={"preference-search"}>
+              <a href={`/1`} className="button">Find me a School</a>
+            </div>
           </div>
         </div>
-      </div>
-    </NewLayout>
+      </NewLayout>
+    </DndProvider>
   );
 }
 
